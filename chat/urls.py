fbf1b6289ec24e9dbb518b0502f2be2e25a656ca
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-06-08 14:41:25 +0300

    send chat messages

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, response
 
 urlpatterns = [
-  path('', index, name='index')
+  path('', index, name='index'),
+  path('response', response, name='response')
 ]
